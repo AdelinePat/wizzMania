125e30e495d0b6b04b457a4356c06fc415e51264
Thibault Caron <thibault.caron@laplateforme.io>
2026-02-27 17:20:55 +0100

    Fix: the text preview addapt to the size of the sidebar instead of expending it

--- client/Headers/widgets/channel_row_widget.hpp
+++ client/Headers/widgets/channel_row_widget.hpp
@@ -1,10 +1,13 @@
 #ifndef CHANNELROWWIDGET_HPP
 #define CHANNELROWWIDGET_HPP
 
+#include <QFontMetrics>
 #include <QHBoxLayout>
 #include <QLabel>
 #include <QMessageBox>
 #include <QPushButton>
+#include <QResizeEvent>
+#include <QSizePolicy>
 #include <QVBoxLayout>
 #include <QWidget>
 
@@ -16,11 +19,18 @@ class ChannelRowWidget : public QWidget {
                             const QString& preview, int unreadCount,
                             bool isGroup, QWidget* parent = nullptr);
 
+ protected:
+  void resizeEvent(QResizeEvent* event) override;
+
  signals:
   void leaveChannelRequested(int64_t channelId);
 
  private:
+  void updatePreviewElision();
+
   int64_t channelId;
+  QString previewText;
+  QLabel* previewLabel = nullptr;
 };
 
 #endif  // CHANNELROWWIDGET_HPP
